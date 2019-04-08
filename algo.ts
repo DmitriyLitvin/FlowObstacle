7b1b7478994a451da3f5fc562294172c78af045a
DmitriyLitvin <dimalitvin192.0@gmail.com>
2019-04-08 20:57:59 +0300

    added style.css visualization.html, stationary.js, algo.ts files (#1)
    * added style.css visualization.html, stationary.js, algo.ts files
    
    * added changes in algo.ts
    
    * added changes in stationary.js
    
    * added fi.png, psi.png
    
    * added constructor.png
    
    * Delete constructor.PNG
    
    * added changes in stationary.js
    
    * added changes in algo.ts
    
    * added changes in algo.ts
    
    * added changes in algo.ts

---
+++ algo.ts
@@ -0,0 +1,377 @@
+class Point {
+    private x: number;
+    private y: number;
+
+    public getX() {
+        return this.x;
+    }
+
+    public getY() {
+        return this.y;
+    }
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static distance(a: Point, b: Point): number {
+        return Math.sqrt(Math.pow(a.getY() - b.getY(), 2) +
+            Math.pow(a.getX() - b.getX(), 2));
+    }
+
+    public static equalityOfPoints(a: Point, b: Point): boolean {
+        return a.getX() == b.getX() && a.getY() == b.getY();
+    }
+
+    public static equationOfLine(x: Point, y: Point, x0: number) {
+        if (y.getX() - x.getX() == 0) {
+            return x0;
+        }
+
+        return (x0 - x.getX()) * (y.getY() - x.getY()) / (y.getX() - x.getX()) + x.getY();
+    }
+}
+
+class Line {
+    private x: Point;
+    private y: Point;
+
+    constructor(x: Point, y: Point) {
+        this.x = new Point(x.getX(), x.getY());
+        this.y = new Point(y.getX(), y.getY());
+    }
+
+    public getPointX() {
+        return this.x;
+    }
+
+    public getPointY() {
+        return this.y;
+    }
+
+    public getLengthOfLine(): number {
+        return Math.sqrt(Math.pow(this.y.getX() - this.x.getX(), 2) +
+            Math.pow(this.y.getY() - this.x.getY(), 2));
+    }
+
+    public static getLineWithTheHighestLength(lines: Line[]) {
+        let line = lines[0];
+
+        for (let i = 0; i < lines.length; i++) {
+            if (line.getLengthOfLine() < lines[i].getLengthOfLine()) {
+                line = lines[i];
+            }
+        }
+
+        return line;
+    }
+}
+
+class Color {
+    private r;
+    private g;
+    private b;
+    private a = 255;
+
+    public constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public getR(): number {
+        return this.r;
+    }
+
+    public getG(): number {
+        return this.g;
+    }
+
+    public getB(): number {
+        return this.b;
+    }
+
+    public getA(): number {
+        return this.a;
+    }
+}
+
+class AlgorithmOfDiscreteFeatures {
+    private alpha;
+    private circulation;
+    private numbOfParts;
+
+    public constructor(alpha: number, circulation: number, numbOfParts: number) {
+        this.alpha = alpha;
+        this.circulation = circulation;
+        this.numbOfParts = numbOfParts;
+    }
+
+    public getAlpha() {
+        return this.alpha;
+    }
+
+    public getCirculation() {
+        return this.circulation;
+    }
+
+    public getNumbOfParts() {
+        return this.numbOfParts;
+    }
+
+    public getBasePoints(): Point[] {
+        let basePoints = [];
+        let numbOnLeftPart = Math.floor(this.numbOfParts / 2);
+        let numbOnRightPart = this.numbOfParts - numbOnLeftPart;
+        let stepForLeftPart = 75 / numbOnLeftPart;
+        let stepForRightPart = 75 / numbOnRightPart;
+        let leftPartOfLine = null;
+        let rightPartOfLine = null;
+        for (let i = 0; i < numbOnLeftPart; i++) {
+            leftPartOfLine = Point.equationOfLine(new Point(300, 500), new Point(375, 250),
+                300 + stepForLeftPart * i);
+            basePoints.push(new Point(300 + stepForLeftPart * i, leftPartOfLine));
+        }
+        for (let i = 0; i <= numbOnRightPart; i++) {
+            rightPartOfLine = Point.equationOfLine(new Point(375, 250), new Point(450, 500),
+                375 + stepForRightPart * i);
+            basePoints.push(new Point(375 + stepForRightPart * i, rightPartOfLine));
+        }
+
+        return basePoints;
+    }
+
+    public getCollocationPoints(): Point[] {
+        let middlePoints = [];
+        let basePoints = this.getBasePoints();
+        for (let i = 0; i < basePoints.length - 1; i++) {
+            let x = (basePoints[i + 1].getX() + basePoints[i].getX()) / 2.0;
+            let y = (basePoints[i + 1].getY() + basePoints[i].getY()) / 2.0;
+            let middlePoint = new Point(x, y);
+            middlePoints.push(middlePoint);
+        }
+
+        return middlePoints;
+    }
+
+    public static getN(points1: Point, points2: Point): Point {
+        let x = (points2.getX() - points1.getX()) / Point.distance(points1, points2);
+        let y = (points2.getY() - points1.getY()) / Point.distance(points1, points2);
+
+        return new Point(-y, x);
+    }
+
+    public getMinSigma(): number {
+        let distances = [];
+        let minDistance;
+        let basePoints = this.getBasePoints();
+
+        for (let i = 0; i < basePoints.length - 1; i++) {
+            distances.push(Point.distance(basePoints[i + 1], basePoints[i]));
+        }
+        minDistance = distances[0];
+        for (let i = 0; i < distances.length; i++) {
+            if (minDistance > distances[i]) {
+                minDistance = distances[i];
+            }
+        }
+
+        return 0.5 * minDistance;
+    }
+
+    public static getR(point: Point, basePoint: Point, sigma: number): number {
+        return Math.max(Point.distance(point, basePoint), sigma);
+    }
+
+    public static getVj(colocatioPoint: Point, basePoint: Point, valueR: number): Point {
+        let firstCom = AlgorithmOfDiscreteFeatures.u(colocatioPoint.getX(),
+            colocatioPoint.getY(), basePoint, valueR);
+        let secondCom = AlgorithmOfDiscreteFeatures.v(colocatioPoint.getX(),
+            colocatioPoint.getY(), basePoint, valueR);
+
+        return new Point(firstCom, secondCom);
+    }
+
+    public static u(x: number, y: number, basePoint: Point, valueR: number): number {
+        let firstCom = (basePoint.getY() - y) / (2 * Math.PI * Math.pow(valueR, 2));
+
+        return firstCom;
+    }
+
+    public static v(x: number, y: number, basePoint: Point, valueR: number): number {
+        let secondCom = (x - basePoint.getX()) / (2 * Math.PI * Math.pow(valueR, 2));
+
+        return secondCom;
+    }
+
+    public getRightPart(): number[] {
+        let rightPart = [];
+        let basePoints = this.getBasePoints();
+        for (let i = 0; i < basePoints.length - 1; i++) {
+            let vectN = AlgorithmOfDiscreteFeatures.getN(basePoints[i], basePoints[i + 1]);
+            rightPart.push(-(vectN.getX() * Math.cos(this.alpha) + vectN.getY() * Math.sin(this.alpha)))
+        }
+        rightPart.push(this.circulation);
+
+        return rightPart;
+    }
+
+    public getLeftPart(): number[][] {
+        let leftPart = [];
+        let sigma = this.getMinSigma();
+        let basePoints = this.getBasePoints();
+        let colocatioPoints = this.getCollocationPoints();
+        for (let i = 0; i < basePoints.length; i++) {
+            let subLeftPart = [];
+            if (i < basePoints.length - 1) {
+                let vectN = AlgorithmOfDiscreteFeatures.getN(basePoints[i], basePoints[i + 1]);
+                for (let j = 0; j < basePoints.length; j++) {
+                    let valueR = AlgorithmOfDiscreteFeatures.getR(colocatioPoints[i], basePoints[j], sigma);
+                    let vectV = AlgorithmOfDiscreteFeatures.getVj(colocatioPoints[i], basePoints[j], valueR);
+                    subLeftPart.push(vectV.getX() * vectN.getX() + vectV.getY() * vectN.getY());
+                }
+            }
+            else {
+                for (let j = 0; j < basePoints.length; j++) {
+                    subLeftPart.push(1);
+                }
+            }
+            leftPart.push(subLeftPart);
+        }
+
+        return leftPart;
+    }
+
+    public getAnotherFi(x: number, y: number, gammas: number[]): number {
+        let sum = 0;
+        let basePoints = this.getBasePoints();
+        let sigma = this.getMinSigma();
+        let circulation = this.circulation;
+        for (let i = 0; i < gammas.length - 1; i++) {
+            let sum1 = 0;
+            for (let k = 0; k <= i; k++) {
+                sum1 += gammas[k];
+            }
+            let rJ = AlgorithmOfDiscreteFeatures.getR(new Point(x, y), basePoints[i], sigma);
+            sum += (sum1 / (2 * Math.PI)) * ((basePoints[i + 1].getY() - basePoints[i].getY()) *
+                (x - basePoints[i].getX()) - (basePoints[i + 1].getX() - basePoints[i].getX()) *
+                (y - basePoints[i].getY())) / Math.pow(rJ, 2)
+        }
+
+        return x * Math.cos(this.alpha) + y * Math.sin(this.alpha) + sum + circulation *
+            Math.atan2(y - basePoints[gammas.length - 1].getY(),
+                x - basePoints[gammas.length - 1].getX()) / (2 * Math.PI);
+    }
+
+
+    public getPsi(x: number, y: number, gammas: number[]): number {
+        let sum = 0;
+        let basePoints = this.getBasePoints();
+        let sigma = this.getMinSigma();
+        for (let i = 0; i < gammas.length; i++) {
+            sum += gammas[i] * Math.log(AlgorithmOfDiscreteFeatures.getR(new Point(x, y),
+                basePoints[i], sigma)) / (2 * Math.PI);
+        }
+
+        return y * Math.cos(this.alpha) - x * Math.sin(this.alpha) - sum;
+    }
+
+    public getGammas(): number[] {
+        let rightPart = this.getRightPart();
+        let leftPart = this.getLeftPart();
+        let gammas = new Gaus(leftPart, rightPart).solve();
+
+        return gammas;
+    }
+
+    public getV(x: number, y: number, gammas: number[]): Point {
+        let sumOfCompsX = 0;
+        let sumOfCompsY = 0;
+        let basePoints = this.getBasePoints();
+        let sigma = this.getMinSigma();
+        for (let i = 0; i < gammas.length; i++) {
+            let valueR = AlgorithmOfDiscreteFeatures.getR(new Point(x, y), basePoints[i], sigma);
+            let sumComp = AlgorithmOfDiscreteFeatures.getVj(new Point(x, y), basePoints[i], valueR);
+            sumOfCompsX = sumOfCompsX + gammas[i] * sumComp.getX();
+            sumOfCompsY = sumOfCompsY + gammas[i] * sumComp.getY();
+        }
+        sumOfCompsX += Math.cos(this.alpha);
+        sumOfCompsY += Math.sin(this.alpha);
+        return new Point(sumOfCompsX, sumOfCompsY);
+    }
+}
+
+interface Solvable {
+    solve();
+}
+
+class Gaus implements Solvable {
+    private a: number[][];
+    private b: number[];
+
+    constructor(a: number[][], b: number[]) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public solve(): number[] {
+        let a = this.a;
+        let b = this.b;
+        let sizeOfMatrix = b.length;
+
+        for (let k = 0; k < sizeOfMatrix; k++) {
+            let d = a[k][k];
+            if (d == 0) {
+                let l = k + 1;
+                while (l != sizeOfMatrix) {
+                    a = this.exchangeLinesOfMatrix(a, l, k);
+                    b = this.exchangeValuesOfVector(b, l, k);
+                    if (a[k][k] != 0) {
+                        break;
+                    }
+                    l++;
+                }
+                if (l == sizeOfMatrix) {
+                    throw new Error("lines of the matrix 'a' are linearly dependent");
+                }
+                d = a[k][k];
+            }
+            a[k][k] = 1;
+            for (let j = k + 1; j < sizeOfMatrix; j++) {
+                a[k][j] = a[k][j] / d;
+            }
+            b[k] = b[k] / d;
+            for (let i = 0; i < sizeOfMatrix; i++) {
+                if (i != k) {
+                    let s = a[i][k];
+                    a[i][k] = 0;
+                    for (let j = k + 1; j < sizeOfMatrix; j++) {
+                        a[i][j] = a[i][j] - s * a[k][j];
+                    }
+                    b[i] = b[i] - s * b[k];
+                }
+            }
+        }
+
+        return b;
+    }
+
+    private exchangeLinesOfMatrix(a: number[][], l: number, k: number): number[][] {
+        for (let i = 0; i < a.length; i++) {
+            let temp = a[l][i];
+            a[l][i] = a[k][i];
+            a[k][i] = temp;
+        }
+
+        return a;
+    }
+
+    private exchangeValuesOfVector(a: number[], l: number, k: number): number[] {
+        let temp = a[l];
+        a[l] = a[k];
+        a[k] = temp;
+
+        return a;
+    }
+}
---
+++ js/stationary.js
@@ -0,0 +1,185 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var canvasData = context.getImageData(0, 0, 750, 750);
+var task = new AlgorithmOfDiscreteFeatures(0, 0, 150);
+var gammas = task.getGammas();
+var lengthOfCanvas = canvasData.height;
+
+function drawLine(a, b, colour, width) {
+    context.beginPath();
+    context.moveTo(a.getX(), a.getY());
+    context.lineTo(b.getX(), b.getY());
+    context.fillStyle = colour;
+    context.fill();
+    context.lineWidth = width;
+    context.strokeStyle = colour;
+    context.stroke();
+}
+
+function drawCircle(point) {
+    context.beginPath();
+    context.arc(point.getX(), point.getY(), 2, 0, 2 * Math.PI, true);
+    context.fillStyle = 'black';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
+}
+
+function generateLines() {
+    var lines = [];
+    for (var i = 10; i < lengthOfCanvas; i += 30) {
+        for (var j = 10; j < lengthOfCanvas; j += 30) {
+            var currentPoint = new Point(i, j);
+            var tempPoint = task.getV(i, j, gammas);
+            var nextPoint = new Point(currentPoint.getX() + tempPoint.getX(),
+                currentPoint.getY() + tempPoint.getY());
+            lines.push(new Line(currentPoint, nextPoint));
+        }
+    }
+    return lines;
+}
+
+function generateV() {
+    var lines = generateLines();
+    var lineWithTheHighestLength = Line.getLineWithTheHighestLength(lines);
+    var newMaxLength = 40;
+    var koef = lineWithTheHighestLength.getLengthOfLine() / newMaxLength;
+    for (var i = 0; i < lines.length; i++) {
+        var endOfLineX = (lines[i].getPointY().getX() - lines[i].getPointX().getX()) /
+            koef + lines[i].getPointX().getX();
+        var endOfLineY = (lines[i].getPointY().getY() - lines[i].getPointX().getY()) /
+            koef + lines[i].getPointX().getY();
+        drawLine(lines[i].getPointX(), new Point(endOfLineX, endOfLineY), 'black', 0.5);
+    }
+}
+
+function discreteColorsGradient(colorsNum) {
+    var colorStep = 510 / (colorsNum - 1);
+    var colors = [];
+    var r = 0;
+    var g = 0;
+    var b = 255;
+    var color = new Color(r, g, b);
+    colors.push(color);
+    var toWhite = true;
+    for (var i = 0; i < colorsNum - 1; i++) {
+        if (toWhite) {
+            r += colorStep;
+            g += colorStep;
+            if (r > 255) {
+                var diff = r - 255;
+                r = 255;
+                g = 255;
+                g -= diff;
+                b -= diff;
+                toWhite = false;
+            }
+        }
+        else {
+            g = Math.max(0, g - colorStep);
+            b = Math.max(0, b - colorStep);
+        }
+        color = new Color(Math.round(r), Math.round(g), Math.round(b));
+        colors.push(color);
+    }
+    return colors;
+}
+
+function getMaxPsi() {
+    var maxPsi = task.getPsi(0, 0, gammas);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            if (maxPsi < task.getPsi(i, j, gammas)) {
+                maxPsi = task.getPsi(i, j, gammas);
+            }
+        }
+    }
+    return maxPsi;
+}
+
+function getMinPsi() {
+    var minPsi = task.getPsi(0, 0, gammas);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            if (minPsi > task.getPsi(i, j, gammas)) {
+                minPsi = task.getPsi(i, j, gammas);
+            }
+        }
+    }
+    return minPsi;
+}
+
+function getMaxAnotherFi() {
+    var maxAnotherFi = task.getAnotherFi(0, 0, gammas);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            if (maxAnotherFi < task.getAnotherFi(i, j, gammas)) {
+                maxAnotherFi = task.getAnotherFi(i, j, gammas);
+            }
+        }
+    }
+    return maxAnotherFi;
+}
+
+function getMinAnotherFi() {
+    var minAnotherFi = task.getAnotherFi(0, 0, gammas);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            if (minAnotherFi > task.getAnotherFi(i, j, gammas)) {
+                minAnotherFi = task.getAnotherFi(i, j, gammas);
+            }
+        }
+    }
+    return minAnotherFi;
+}
+
+function drawPixel(i, j, r, g, b, a) {
+    var index = (i + j * lengthOfCanvas) * 4;
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+function updateCanvas() {
+    context.putImageData(canvasData, 0, 0);
+}
+
+function getIndexOfColor(currentVal, minVal, diapason, colorsLength) {
+    return Math.floor(Math.round(((currentVal - minVal) / diapason) * (colorsLength - 1)));
+}
+
+function psi(numbOfColors) {
+    var minPsi = getMinPsi();
+    var maxPsi = getMaxPsi();
+    var diapason = maxPsi - minPsi;
+    var colors = discreteColorsGradient(numbOfColors);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            var indOfColor = getIndexOfColor(task.getPsi(i, j, gammas), minPsi, diapason, colors.length);
+            var color = colors[indOfColor];
+            drawPixel(i, j, color.getR(), color.getG(), color.getB(), color.getA())
+        }
+    }
+
+    updateCanvas();
+}
+
+function fi(numbOfColors) {
+    var minAnotherFi = getMinAnotherFi();
+    var maxAnotherFi = getMaxAnotherFi();
+    var diapason = maxAnotherFi - minAnotherFi;
+    var colors = discreteColorsGradient(numbOfColors);
+    for (var i = 0; i < lengthOfCanvas; i++) {
+        for (var j = 0; j < lengthOfCanvas; j++) {
+            var indOfColor = getIndexOfColor(task.getAnotherFi(i, j, gammas), minAnotherFi, diapason, colors.length);
+            var color = colors[indOfColor];
+            drawPixel(i, j, color.getR(), color.getG(), color.getB(), color.getA())
+        }
+    }
+
+    updateCanvas();
+}
+
+fi(25);
